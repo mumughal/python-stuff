5e3405d42e62b662f5e185c74406c0ac46f86213
mumughal <muneeba.mughal@grandcentrix.net>
2018-02-23 00:46:36 +0100

    Add passing tests

--- test_task3.py
+++ test_task3.py
@@ -5,10 +5,10 @@ from task3 import generate_y_values
 class TestStringMethods(unittest.TestCase):
 
     def test_calculate1(self):
-        self.assertEqual(generate_y_values("(5 + 8) * 3/8 +3", [1,2,3,4]), 7.875)
+        self.assertEqual(generate_y_values("2 * x + 1", [1,2,3,4]), [3, 5, 7, 9])
 
     def test_calculate2(self):
-        self.assertEqual(generate_y_values("(15 + 18) - 89", [1,2,3,4]), -56)
+        self.assertEqual(generate_y_values("(15 + 18) - 89", [1,2,3,4]), [-56, -56, -56, -56])
 
 
 if __name__ == '__main__':
